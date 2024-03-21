13ab0d521ed769e8086290c8971c8fc423b33d34
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-03-21 21:02:40 +0200

    feat(global): add redis tmp to gitignore

--- backend/apps/api/src/app.module.ts
+++ backend/apps/api/src/app.module.ts
@@ -4,17 +4,17 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/dist/esm/plugin/landingPage/default';
+// import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/dist/esm/plugin/landingPage/default';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      playground: true,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-    }),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    //   sortSchema: true,
+    //   playground: true,
+    //   plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
