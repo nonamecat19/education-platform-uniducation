d722a7f3d968264e1b9690414b02a03643715799
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-03-23 18:35:44 +0200

    chore(global): delete old backend

--- backend/apps/schedule/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ScheduleModule } from './schedule.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ScheduleModule);
-  await app.listen(3000);
-}
-bootstrap();
