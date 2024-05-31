cc0231d869438a4394e483b81f7142479f771d06
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-05-31 20:22:19 +0300

    feat: add log out button to all headers

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -6,6 +6,7 @@ import classNames from 'classnames'
 import { HeaderLinks } from '@/lib/types'
 import Image from 'next/image'
 import logo from '@/public/svg/logo.svg'
+import SignOutBtn from '@/components/auth/SignOutBtn'
 
 interface Props {
   links: HeaderLinks
@@ -16,35 +17,39 @@ export const Header = ({ links }: Props) => {
 
   return (
     <div className="pb-3 text-xl">
-      <header className="flex gap-5 p-3 items-center">
-        <Link href="/">
-          <div className="flex gap-2 mr-5 items-center border border-indigo-500 rounded-lg px-3 py-1">
-            <Image
-              src={logo}
-              alt="logo"
-              height={34}
-            />
-            <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-indigo-500 to-indigo-600">
-              Uniducation
-            </h2>
-          </div>
-        </Link>
+      <header className="flex justify-between items-center pr-5">
+        <div className="flex gap-5 p-3 items-center">
+          <Link href="/">
+            <div className="flex gap-2 mr-5 items-center border border-indigo-500 rounded-lg px-3 py-1">
+              <Image
+                src={logo}
+                alt="logo"
+                height={34}
+              />
+              <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-indigo-500 to-indigo-600">
+                Uniducation
+              </h2>
+            </div>
+          </Link>
 
-        {links.map(({ url, name }) => {
-          return (
-            <Link
-              key={url}
-              href={url}
-              className={classNames(
-                'font-semibold',
-                {
-                  'text-indigo-400': pathname === url,
-                })}
-            >
-              {name}
-            </Link>
-          )
-        })}
+          {links.map(({ url, name }) => {
+            return (
+              <Link
+                key={url}
+                href={url}
+                className={classNames(
+                  'font-semibold',
+                  {
+                    'text-indigo-400': pathname === url,
+                  })}
+              >
+                {name}
+              </Link>
+            )
+          })}
+        </div>
+
+        <SignOutBtn/>
       </header>
       <Separator />
     </div>
