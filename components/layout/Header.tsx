9b614d7890c1233d2e97b2405be5c8eb27c2fd84
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-05-31 09:20:49 +0300

    feat: add logo to header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import classNames from 'classnames'
 import { HeaderLinks } from '@/lib/types'
+import Image from 'next/image'
+import logo from '@/public/svg/logo.svg'
 
 interface Props {
   links: HeaderLinks
@@ -13,8 +15,21 @@ export const Header = ({ links }: Props) => {
   const pathname = usePathname()
 
   return (
-    <div className="pb-3">
-      <header className="flex gap-5 p-3">
+    <div className="pb-3 text-xl">
+      <header className="flex gap-5 p-3 items-center">
+        <Link href="/">
+          <div className="flex gap-2 mr-5 items-center border border-indigo-500 rounded-lg px-3 py-1">
+            <Image
+              src={logo}
+              alt="logo"
+              height={34}
+            />
+            <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-indigo-500 to-indigo-600">
+              Uniducation
+            </h2>
+          </div>
+        </Link>
+
         {links.map(({ url, name }) => {
           return (
             <Link
