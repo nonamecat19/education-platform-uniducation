0ba6519b6fd564b78ddd4418f254416626a8d437
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-05-31 20:19:28 +0300

    feat: show only pages based on user role

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,41 @@
 import Link from 'next/link'
-import { getUserAuth } from '@/lib/auth/utils'
+import { getCurrentRole, getUserAuth } from '@/lib/auth/utils'
 import SignOutBtn from '@/components/auth/SignOutBtn'
+import { UserRole } from '@/lib/eunms'
 
 export default async function LandingPage() {
   const { session } = await getUserAuth()
+
+  const role = await getCurrentRole()
 
   return (
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <nav className="ml-auto flex gap-4 sm:gap-6">
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/student"
-          >
-            Student
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/admin/dashboard"
-          >
-            Admin
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/teacher"
-          >
-            Teacher
-          </Link>
+          {role === UserRole.Student && (
+            <Link
+              className="text-lg font-medium hover:underline underline-offset-4"
+              href="/student"
+            >
+              Go to student page
+            </Link>
+          )}
+          {role === UserRole.Admin && (
+            <Link
+              className="text-lg font-medium hover:underline underline-offset-4"
+              href="/admin/dashboard"
+            >
+              Go to admin page
+            </Link>
+          )}
+          {role === UserRole.Teacher && (
+            <Link
+              className="text-lg font-medium hover:underline underline-offset-4"
+              href="/teacher"
+            >
+              Go to teacher page
+            </Link>
+          )}
           {!!session ? (
             <SignOutBtn />
           ) : (
