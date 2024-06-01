543f24f19c1ce943863cd49a0e74b26824048b52
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-06-01 15:19:57 +0300

    feat: improve student and teacher profile page

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,20 +1,43 @@
 import { trpcSSR } from '@/lib/trpc/ssr'
+import { getCurrentUser } from '@/lib/api/users/queries'
 
 export default async function StudentsPage() {
   const { student } = await trpcSSR.students.getCurrentStudent()
+  const { user } = await getCurrentUser()
 
-  if (!student) {
+  if (!student || !user) {
     return (
       <div>Student not found</div>
     )
   }
 
   return (
-    <div>
-      <div>Name: {student?.name}</div>
-      <div>Surname: {student?.surname}</div>
-      <div>Patronymic: {student?.patronymic}</div>
-      <div>StudentID: {student?.stuentId}</div>
+    <div
+      className="p-4 rounded-sm shadow-sm text-secondary-foreground break-all whitespace-break-spaces flex gap-5"
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={user?.image ?? ''}
+        alt="avatar"
+        className="h-28 rounded-lg"
+      />
+      <div>
+        <div>
+          Name: {student?.name}
+        </div>
+        <div>
+          Surname: {student?.surname}
+        </div>
+        <div>
+          Patronymic: {student?.patronymic}
+        </div>
+        <div>
+          Profession: {student?.stuentId}
+        </div>
+        <div>
+          Email: {user.email}
+        </div>
+      </div>
     </div>
   )
 }
--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -1,21 +1,43 @@
 import { trpcSSR } from '@/lib/trpc/ssr'
+import { getCurrentUser } from '@/lib/api/users/queries'
 
 export default async function TeacherPage() {
   const { teacher } = await trpcSSR.teachers.getCurrentTeacher()
+  const { user } = await getCurrentUser()
 
-  if (!teacher) {
+  if (!teacher || !user) {
     return (
       <div>Teacher not found</div>
     )
   }
 
   return (
-    <div>
-      Name: {teacher?.name}
-      Surname: {teacher?.surname}
-      Patronymic: {teacher?.patronymic}
-      Profession: {teacher?.profession}
-      <div>asdf</div>
+    <div
+      className="p-4 rounded-sm shadow-sm text-secondary-foreground break-all whitespace-break-spaces flex gap-5"
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={user?.image ?? ''}
+        alt="avatar"
+        className="h-28 rounded-lg"
+      />
+      <div>
+        <div>
+          Name: {teacher?.name}
+        </div>
+        <div>
+          Surname: {teacher?.surname}
+        </div>
+        <div>
+          Patronymic: {teacher?.patronymic}
+        </div>
+        <div>
+          Profession: {teacher?.profession}
+        </div>
+        <div>
+          Email: {user.email}
+        </div>
+      </div>
     </div>
   )
 }
