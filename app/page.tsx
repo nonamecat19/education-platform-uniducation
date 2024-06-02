293ab642f2244c0a57b41b1a7dc2f513f78cdbde
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-06-02 18:17:44 +0300

    feat: change main page to redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,14 @@
-import Link from 'next/link'
 import { getCurrentRole, getUserAuth } from '@/lib/auth/utils'
-import SignOutBtn from '@/components/auth/SignOutBtn'
-import { UserRole } from '@/lib/eunms'
+import { redirect } from 'next/navigation'
 
 export default async function LandingPage() {
   const { session } = await getUserAuth()
 
+  if (!session) {
+    redirect('/api/auth/signin?callbackUrl=%2Fcomplete-register')
+  }
+
   const role = await getCurrentRole()
 
-  return (
-    <div className="flex flex-col min-h-screen">
-      <header className="px-4 lg:px-6 h-14 flex items-center">
-        <nav className="ml-auto flex gap-4 sm:gap-6">
-          {role === UserRole.Student && (
-            <Link
-              className="text-lg font-medium hover:underline underline-offset-4"
-              href="/student"
-            >
-              Go to student page
-            </Link>
-          )}
-          {role === UserRole.Admin && (
-            <Link
-              className="text-lg font-medium hover:underline underline-offset-4"
-              href="/admin/dashboard"
-            >
-              Go to admin page
-            </Link>
-          )}
-          {role === UserRole.Teacher && (
-            <Link
-              className="text-lg font-medium hover:underline underline-offset-4"
-              href="/teacher"
-            >
-              Go to teacher page
-            </Link>
-          )}
-          {!!session ? (
-            <SignOutBtn />
-          ) : (
-            <Link
-              className="text-sm font-medium hover:underline underline-offset-4"
-              href="/sign-in"
-            >
-              Sign In
-            </Link>
-          )}
-        </nav>
-      </header>
-    </div>
-  )
+  redirect(`/${role}`)
 }
