d2d198fc814db138ac13bc3ed3cf58ac851abab9
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-06-09 23:27:48 +0300

    fix: user can delete section bug

--- app/student/courses/[id]/page.tsx
+++ app/student/courses/[id]/page.tsx
@@ -28,7 +28,6 @@ export default async function CourseIdPage({ params }: Params) {
         <UnitBlock key={unit.id} value={unit}>
           {unit.textSections.map((textSection) => (
             <TextSectionBlock
-              isEditable
               key={textSection.id}
               value={textSection}
             />
